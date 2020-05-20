f6dea53812eb12ff64ea9fd9006e41c5ca5f9887
wayasmiguel <wayasmiguel@outlook.com>
2020-05-20 17:12:02 -0500

    Error en populate corregido

--- app/http/models/Appliance.js
+++ app/http/models/Appliance.js
@@ -5,12 +5,12 @@ var utc = new Date(now.getTime() - now.getTimezoneOffset() * 60000).toISOString(
 
 const applianceSchema = new Schema({
   idClient:  [{ type: Schema.Types.ObjectId, ref: 'Client' }],
-  idDocuments:  [{ type: Schema.Types.ObjectId, ref: 'Documents', autopopulate: true }],
-  idAmount: [{ type: Schema.Types.ObjectId, ref: 'Amount', autopopulate: true }],
-  idGeneralInfo:  [{ type: Schema.Types.ObjectId, ref: 'GeneralInfo', autopopulate: true }],
-  idComercialInfo:  [{ type: Schema.Types.ObjectId, ref: 'ComercialInfo', autopopulate: true }],
+  idDocuments:  [{ type: Schema.Types.ObjectId, ref: 'Documents' }],
+  idAmount: [{ type: Schema.Types.ObjectId, ref: 'Amount'}],
+  idGeneralInfo:  [{ type: Schema.Types.ObjectId, ref: 'GeneralInfo' }],
+  idComercialInfo:  [{ type: Schema.Types.ObjectId, ref: 'ComercialInfo' }],
   registerDate: { type: Date, default: utc },
-  proposals: [{ type: Schema.Types.ObjectId, ref: 'Proposal', autopopulate: true }],
+  proposals: [{ type: Schema.Types.ObjectId, ref: 'Proposal' }],
   reason: String,
   tips: String,
   status: { type: Boolean, default: false }
---
+++ app/http/services/fileManager.js
@@ -0,0 +1,210 @@
+'use strict'
+
+const fs = require('fs');
+const path = require("path");
+require('dotenv').config({
+    path: `.env.${process.env.NODE_ENV}`
+});
+const S3 = require('../../../config/s3/buckets');
+const uploadDir = "../../../public/tmpFiles/";
+
+const moveFile = async(files) => {
+    const UploadFiles = Object.keys(files).map(async(key) => {
+        return new Promise((resolve, reject) => {
+            let filePath = path.resolve(__dirname, uploadDir + files[key].name);
+            const callback = (error, success) => {
+                if(error){
+                    console.log("Something went wrong trying to upload the files to the server");
+                }
+                resolve(filePath);
+              };
+              files[key].mv(filePath, callback);
+        });
+    });
+
+    let uploadedFiles = await Promise.all(UploadFiles);
+
+    // Object.keys(files).forEach(async(key) => {
+    //     const filePath = path.resolve(__dirname, uploadDir + files[key].name);
+    //         files[key].mv(filePath, (error) => {
+    //             if(error){
+    //                 return error;
+    //             }
+    //         });     
+    //     //uploadedFiles.push(uploadDir + files[key].name);
+    //     //uploadedFiles.push(filePath);
+    // });
+
+    return uploadedFiles;
+};
+
+const getContentType = async(files) => {
+    let types = [];
+
+    try{
+
+        Object.keys(files).forEach(key => {
+            let rc = 'application/octet-stream';
+            const fn = files[key].name.toLowerCase();
+            if (fn.indexOf('.pdf') >= 0) rc = 'application/pdf';
+            else if (fn.indexOf('.zip') >= 0) rc = 'application/zip';
+            else if (fn.indexOf('.png') >= 0) rc = 'image/png';
+            else if (fn.indexOf('.jpg') >= 0 || fn.indexOf('.jpeg') >= 0) rc = 'image/jpg';
+            else if (fn.indexOf('.tiff') >= 0 || fn.indexOf('.tif') >= 0) rc = 'image/tiff';
+    
+            types.push(rc);
+        });
+    
+        return types;
+
+    }
+    catch(error){
+        console.log("Something went wrong trying to get the content types");
+    }
+};
+
+const getBase64 = async(filesPath) => {
+    let bases = [];
+
+    filesPath.forEach( file => {
+        try{
+            const bitmap = fs.readFileSync(file);
+            bases.push(Buffer.from(bitmap, 'base64'));
+        } 
+        catch(error){
+            console.log("Something went wrong trying to get the base 64");
+        }
+    })
+
+    return bases;
+};
+
+const setNewName = async(files) => {
+    let names = [];
+
+    try{
+
+        Object.keys(files).forEach(key => {
+            names.push(`${new Date().getTime()}-${files[key].name}`);
+        });
+
+        return names;
+
+    }
+    catch(error){
+        console.log("Something went wrong trying to set the new files names");
+    }
+
+};
+
+const uploadFile = async(filesName, filesBase64, contentsType) => {
+    try{
+        const UploadFiles = filesName.map(async(name, key) => {
+            return await uploadFileS3(name, filesBase64[key], contentsType[key]);
+        });
+
+        let FilesUploaded = await Promise.all(UploadFiles);
+        deleteFromServer();
+        return FilesUploaded;
+    }
+    catch(error){
+        console.log("Something went wrong trying to upload the files to S3");
+    }
+};
+
+const deleteFromServer = async() => {
+    try{
+
+        fs.readdir(path.resolve(__dirname, uploadDir), (error, files) => {
+            if(error) throw error;
+            
+            for(const file of files) {
+                fs.unlink(path.join(path.resolve(__dirname, uploadDir), file), error => {
+                    if (error) throw error;
+                });
+            }
+        });
+
+    }
+    catch(error){
+        console.log("Something went wrong trying to delete the files from the server");
+    }
+}
+
+const deleteFromS3 = async(files) => {
+    //return new Promise((resolve, reject) => {
+
+        try{
+            //console.log(files.file);
+            const bucket = S3.bucket();
+            // const callback = (error, data) => {
+            // if(error){
+            //     reject(error);
+            //     return;
+            // }
+            //     console.log(data);
+            //     resolve(data);
+            // };
+            var params = {
+                Key: files.file
+            };
+            console.log(params);
+            bucket.deleteObject(params,(err, data) => {
+                console.log(err);
+                console.log(data);
+            });
+        }
+        catch(error){
+            console.log("Something went wrong trying to delete a file from S3");
+        }
+    //});
+}
+
+const uploadFileS3 = async(name, base64, contentType) => {
+    return new Promise((resolve, reject) => {
+      const params = {
+        Key: `${name}`, ACL: 'public-read', Body: base64, ContentType: contentType,
+      };
+      const bucket = S3.bucket();
+      const callback = (error, data) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve(data.Location);
+      };
+      bucket.upload(params, callback);
+    });
+}
+
+
+
+const filesUploadCore = async(files) => {
+
+    return new Promise(async(resolve, reject) => {
+
+        try{
+
+            const filesPath = await moveFile(files);
+            const contentsType = await getContentType(files);
+            const filesBase64 = await getBase64(filesPath);
+            const filesName = await setNewName(files);
+            const urls = await uploadFile(filesName, filesBase64, contentsType);
+
+            resolve(urls);
+
+        }
+        catch(error){
+            console.log("Something went wrong in the files upload core");
+        }
+
+    });
+
+
+}
+
+//moveFile, getContentType, getBase64, setNewName, uploadFile
+
+module.exports = {
+    filesUploadCore, deleteFromS3
+};
