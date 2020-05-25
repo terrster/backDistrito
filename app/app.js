1ec26deb44a447ca34207348dcc3ea43e9169f0a
BrandonGarcia <brandongs180@gmail.com>
2020-05-24 22:21:21 -0500

    Agregar cors

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const fileUpload = require('express-fileupload');
+const cors = require('cors');
 
 const app = express();
 
@@ -20,13 +21,7 @@ app.use(fileUpload({
 }));
 
 //CORS
-app.use((request, response, next) => {
-    response.header('Access-Control-Allow-Origin', '*');
-    response.header('Access-Control-Allow-Headers', 'token, Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    response.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    response.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
+app.use(cors());
 
 //Prefix or routes
 app.use(webRoutes);
