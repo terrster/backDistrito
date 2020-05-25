6751f34ac15aa9d54b8c08a59e269c1201ce7ce6
wayasmiguel <wayasmiguel@outlook.com>
2020-05-24 23:49:33 -0500

    configuraciones aws

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const fileUpload = require('express-fileupload');
-const cors = require('cors');
+//const cors = require('cors');
 
 const app = express();
 
@@ -21,7 +21,14 @@ app.use(fileUpload({
 }));
 
 //CORS
-app.use('*', cors());
+app.use((request, response, next) => {
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Headers', 'token, Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    response.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    response.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 //Prefix or routes
 app.use(webRoutes);
