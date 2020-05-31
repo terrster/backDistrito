9ed16be93f2da71b73efc8b4936fcfe86a0ad914
wayasmiguel <wayasmiguel@outlook.com>
2020-05-31 16:51:07 -0500

    documentsController@update corregido

--- app/http/controllers/documentsController.js
+++ app/http/controllers/documentsController.js
@@ -112,7 +112,7 @@ const documentsController = {
             let user = await User.findById(idUser);
 
             if(user){
-                let dealUpdated = await hubspotController.deal.update(user.hubspotDealId, 'documents', filesParams);
+                var dealUpdated = await hubspotController.deal.update(user.hubspotDealId, 'documents', filesParams);
 
                 if(dealUpdated.error){
                     return response.json({
@@ -129,7 +129,150 @@ const documentsController = {
                 });
             }
 
-            await Documents.findByIdAndUpdate(id, { $push : filesUploaded });
+            let filesParamsDocs = {
+                oficialID: [],
+                proofAddress: [],
+                bankStatements: [],
+                constitutiveAct: [],
+                otherActs: [],
+                financialStatements: [],
+                rfc: [],
+                lastDeclarations: [],
+                acomplishOpinion: [],
+                facturacion: [],
+                others: [],
+                cventerprise: [],
+                proofAddressMainFounders: []
+            };
+
+            //oficialID
+            if(dealUpdated.properties.n9_1_id && dealUpdated.properties.n9_1_id.value != ''){
+                filesParamsDocs.oficialID[0] = dealUpdated.properties.n9_1_id.value;
+            }
+            if(dealUpdated.properties.n9_1_2_id && dealUpdated.properties.n9_1_2_id.value != ''){
+                filesParamsDocs.oficialID[1] = dealUpdated.properties.n9_1_2_id.value;
+            }
+            if(dealUpdated.properties.n9_1_3_id && dealUpdated.properties.n9_1_3_id.value != ''){
+                filesParamsDocs.oficialID[2] = dealUpdated.properties.n9_1_3_id.value;
+            }
+            if(dealUpdated.properties.n9_1_4_id && dealUpdated.properties.n9_1_4_id.value != ''){
+                filesParamsDocs.oficialID[3] = dealUpdated.properties.n9_1_4_id.value;
+            }
+
+            //proofAddress
+            if(dealUpdated.properties.n9_2_comp_domicilio && dealUpdated.properties.n9_2_comp_domicilio.value != ''){
+                filesParamsDocs.proofAddress[0] = dealUpdated.properties.n9_2_comp_domicilio.value;
+            }
+            if(dealUpdated.properties.n9_2_1_comp_domicilio_2 && dealUpdated.properties.n9_2_1_comp_domicilio_2.value != ''){
+                filesParamsDocs.proofAddress[1] = dealUpdated.properties.n9_2_1_comp_domicilio_2.value;
+            }
+            if(dealUpdated.properties.n9_2_2_comp_domicilio_3 && dealUpdated.properties.n9_2_2_comp_domicilio_3.value != ''){
+                filesParamsDocs.proofAddress[2] = dealUpdated.properties.n9_2_2_comp_domicilio_3.value;
+            }
+
+            //bankStatements
+            if(dealUpdated.properties.n9_3_estados_de_cuenta && dealUpdated.properties.n9_3_estados_de_cuenta.value != ''){
+                filesParamsDocs.bankStatements[0] = dealUpdated.properties.n9_3_estados_de_cuenta.value;
+            }
+            if(dealUpdated.properties.n9_3_1_estados_de_cuenta && dealUpdated.properties.n9_3_1_estados_de_cuenta.value != ''){
+                filesParamsDocs.bankStatements[1] = dealUpdated.properties.n9_3_1_estados_de_cuenta.value;
+            }
+            if(dealUpdated.properties.n9_3_2_estados_de_cuenta && dealUpdated.properties.n9_3_2_estados_de_cuenta.value != ''){
+                filesParamsDocs.bankStatements[2] = dealUpdated.properties.n9_3_2_estados_de_cuenta.value;
+            }
+            if(dealUpdated.properties.n9_3_3_estados_de_cuenta && dealUpdated.properties.n9_3_3_estados_de_cuenta.value != ''){
+                filesParamsDocs.bankStatements[3] = dealUpdated.properties.n9_3_3_estados_de_cuenta.value;
+            }
+            if(dealUpdated.properties.n9_3_4_estados_de_cuenta && dealUpdated.properties.n9_3_4_estados_de_cuenta.value != ''){
+                filesParamsDocs.bankStatements[4] = dealUpdated.properties.n9_3_4_estados_de_cuenta.value;
+            }
+            if(dealUpdated.properties.n9_3_5_estados_de_cuenta && dealUpdated.properties.n9_3_5_estados_de_cuenta.value != ''){
+                filesParamsDocs.bankStatements[5] = dealUpdated.properties.n9_3_5_estados_de_cuenta.value;
+            }
+            if(dealUpdated.properties.n9_3_6_estados_de_cuenta && dealUpdated.properties.n9_3_6_estados_de_cuenta.value != ''){
+                filesParamsDocs.bankStatements[6] = dealUpdated.properties.n9_3_6_estados_de_cuenta.value;
+            }
+            if(dealUpdated.n9_3_7_estados_de_cuenta && dealUpdated.properties.n9_3_7_estados_de_cuenta.value != ''){
+                filesParamsDocs.bankStatements[7] = dealUpdated.properties.n9_3_7_estados_de_cuenta.value;
+            }
+            if(dealUpdated.properties.n9_3_8_estados_de_cuenta && dealUpdated.properties.n9_3_8_estados_de_cuenta.value != ''){
+                filesParamsDocs.bankStatements[8] = dealUpdated.properties.n9_3_8_estados_de_cuenta.value;
+            }
+            if(dealUpdated.properties.n9_3_9_estados_de_cuenta && dealUpdated.properties.n9_3_9_estados_de_cuenta.value != ''){
+                filesParamsDocs.bankStatements[9] = dealUpdated.properties.n9_3_9_estados_de_cuenta.value;
+            }
+            if(dealUpdated.properties.n9_3_10_estados_de_cuenta && dealUpdated.properties.n9_3_10_estados_de_cuenta.value != ''){
+                filesParamsDocs.bankStatements[10] = dealUpdated.properties.n9_3_10_estados_de_cuenta.value;
+            }
+            if(dealUpdated.properties.n9_3_11_estados_de_cuenta && dealUpdated.properties.n9_3_11_estados_de_cuenta.value != ''){
+                filesParamsDocs.bankStatements[11] = dealUpdated.properties.n9_3_11_estados_de_cuenta.value;
+            }
+
+            //constitutiveAct
+            if(dealUpdated.properties.n9_9_acta_constitutiva && dealUpdated.properties.n9_9_acta_constitutiva.value != ''){
+                filesParamsDocs.constitutiveAct[0] = dealUpdated.properties.n9_9_acta_constitutiva.value;
+            }
+
+            //otherActs
+            if(dealUpdated.properties.n9_8_otros && dealUpdated.properties.n9_8_otros.value != ''){
+                filesParamsDocs.otherActs[0] = dealUpdated.properties.n9_8_otros.value;
+            }
+
+            //financialStatements
+            if(dealUpdated.properties.n9_93_1_eeff && dealUpdated.properties.n9_93_1_eeff.value != ''){
+                filesParamsDocs.financialStatements[0] = dealUpdated.properties.n9_93_1_eeff.value;
+            }
+            if(dealUpdated.properties.n9_93_1_1_eeff && dealUpdated.properties.n9_93_1_1_eeff.value != ''){
+                filesParamsDocs.financialStatements[1] = dealUpdated.properties.n9_93_1_1_eeff.value;
+            }
+            if(dealUpdated.properties.n9_93_2_eeff && dealUpdated.properties.n9_93_2_eeff.value != ''){
+                filesParamsDocs.financialStatements[2] = dealUpdated.properties.n9_93_2_eeff.value;
+            }
+
+            //rfc
+            if(dealUpdated.properties.n9_4_rfc && dealUpdated.properties.n9_4_rfc.value != ''){
+                filesParamsDocs.rfc[0] = dealUpdated.properties.n9_4_rfc.value;
+            }
+
+            //lastDeclarations
+            if(dealUpdated.properties.n9_5_declaraci_n && dealUpdated.properties.n9_5_declaraci_n.value != ''){
+                filesParamsDocs.lastDeclarations[0] = dealUpdated.properties.n9_5_declaraci_n.value;
+            }
+            if(dealUpdated.properties.n9_5_1_declaraci_n && dealUpdated.properties.n9_5_1_declaraci_n.value != ''){
+                filesParamsDocs.lastDeclarations[1] = dealUpdated.properties.n9_5_1_declaraci_n.value;
+            }
+            if(dealUpdated.properties.n9_5_2_declaraci_n && dealUpdated.properties.n9_5_2_declaraci_n.value != ''){
+                filesParamsDocs.lastDeclarations[2] = dealUpdated.properties.n9_5_2_declaraci_n.value;
+            }
+            if(dealUpdated.properties.n9_5_3_declaraci_n && dealUpdated.properties.n9_5_3_declaraci_n.value != ''){
+                filesParamsDocs.lastDeclarations[3] = dealUpdated.properties.n9_5_3_declaraci_n.value;
+            }
+
+            //acomplishOpinion
+            if(dealUpdated.properties.n9_6_opini_n_de_cumplimiento && dealUpdated.properties.n9_6_opini_n_de_cumplimiento.value != ''){
+                filesParamsDocs.acomplishOpinion[0] = dealUpdated.properties.n9_6_opini_n_de_cumplimiento.value;
+            }
+
+            //facturacion
+            if(dealUpdated.properties.n9_7_xmls && dealUpdated.properties.n9_7_xmls.value != ''){
+                filesParamsDocs.facturacion[0] = dealUpdated.properties.n9_7_xmls.value;
+            }
+
+            //others
+            if(dealUpdated.properties.n9_91_reporte_de_cr_dito && dealUpdated.properties.n9_91_reporte_de_cr_dito.value != ''){
+                filesParamsDocs.others[0] = dealUpdated.properties.n9_91_reporte_de_cr_dito.value;
+            }
+            if(dealUpdated.properties.n9_92_1_escritura && dealUpdated.properties.n9_92_1_escritura.value != ''){
+                filesParamsDocs.others[1] = dealUpdated.properties.n9_92_1_escritura.value;
+            }
+            if(dealUpdated.properties.n9_92_2_escritura && dealUpdated.properties.n9_92_2_escritura.value != ''){
+                filesParamsDocs.others[2] = dealUpdated.properties.n9_92_2_escritura.value;
+            }
+            if(dealUpdated.properties.n9_92_3_escritura && dealUpdated.properties.n9_92_3_escritura.value != ''){
+                filesParamsDocs.others[3] = dealUpdated.properties.n9_92_3_escritura.value;
+            }
+
+            await Documents.findByIdAndUpdate(id, {$set: filesParamsDocs},{multi: true});
 
             user = await User.findById(idUser);
 
@@ -139,7 +282,7 @@ const documentsController = {
                 user: user
             });
         }
-        catch(error){
+        catch(error){console.log(error)
             return response.json({
                 code: 500,
                 msg: "Algo salió mal tratando de cargar documentos",
