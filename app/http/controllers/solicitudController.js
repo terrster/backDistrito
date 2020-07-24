4d31eb220263e7d374ac0fbb431f6cad606326f8
wayasmiguel <wayasmiguel@outlook.com>
2020-07-23 21:11:04 -0500

    html pdf solicitud

--- app/http/controllers/solicitudController.js
+++ app/http/controllers/solicitudController.js
@@ -11,7 +11,7 @@ const solicitudController = {
         const hubspot = await hubspotController.deal.show(request.params.id);
 
         const content = `
-        <html style="zoom: 1;">
+        <html style="zoom: 0.55;">
         <head></head>
         <body>
           <div>
