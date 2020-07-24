458c55c5b9edd5daf06c53eb11efb15ed8c2d6e9
wayasmiguel <wayasmiguel@outlook.com>
2020-07-23 20:41:59 -0500

    html pdf solicitud

--- app/http/controllers/solicitudController.js
+++ app/http/controllers/solicitudController.js
@@ -141,7 +141,7 @@ const solicitudController = {
 
         try{
             await pdf.create(content).toBuffer(function(error, buffer){
-                //response.header("Access-Control-Allow-Origin", "*");
+                response.header("Access-Control-Allow-Origin", "*");
                 response.header("Access-Control-Allow-Headers", "X-Requested-With");
                 response.header('content-type', 'application/pdf');
                 return response.send(buffer);
