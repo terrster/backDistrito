e977744a991f628cf3591ff0a16987b1fffb3ea0
wayasmiguel <wayasmiguel@outlook.com>
2020-11-05 11:02:59 -0600

    prueba circulo

--- app/http/controllers/circuloCreditoController.js
+++ app/http/controllers/circuloCreditoController.js
@@ -1,7 +1,15 @@
 'use strict'
 
 const hubspotController = require("../controllers/hubspotController");
-
+const _axios = require("axios").default;
+const axios = _axios.create({
+    baseURL: 'https://services.circulodecredito.com.mx/sandbox/v2/',
+    headers: {
+        'Content-Type': 'application/json',
+        'x-api-key' : 'uh1Z7TIGfqwBimnRKCx9AORVZCm3yvH9'
+    }
+});
+const moment = require("moment");
 
 const circuloCreditoController = {
 
@@ -9,28 +17,33 @@ const circuloCreditoController = {
         let id = request.params.id;//id de client - hubspot
 
         try{
-            let data = await hubspotController.deal.show(id);
+            let hubspot = await hubspotController.deal.show(id);
+            let estado = hubspot.properties.estado_de_la_rep_de_la_persona.value.toUpperCase();
+            let birthDate = new Date(hubspot.properties.n4_5_fecha_de_nacimiento.value);
+            birthDate = moment(birthDate).format('YYYY-DD-MM');
 
             let params = {
-                apellidoPaterno: data.properties.n4_2_apellido_paterno.value,
-                apellidoMaterno: data.properties.n4_3_apellido_materno.value,
-                primerNombre: data.properties.n5_4_nombre.value,
-                fechaNacimiento: data.properties.n4_5_fecha_de_nacimiento.value,
-                RFC: data.properties.n3_rfc.value,
-                nacionalidad: "Pendiente",
-                domicilio: {
-                    direccion: "Calle: " + data.properties.n4_6_calle.value + " Ext: " + data.properties.n4_7_num_ext.value + " Int: " + data.properties.n4_8_num_int.value,
-                    coloniaPoblacion: data.properties.n4_91_colonia.value,
-                    delegacionMunicipio: data.properties.municipio_de_la_persona.value,
-                    ciudad: "Pendiente",
-                    estado: data.properties.estado_de_la_rep_de_la_persona.value,
-                    CP: data.properties.n4_9_c_p_.value
+                "apellidoPaterno": hubspot.properties.n4_2_apellido_paterno.value.toUpperCase(),
+                "apellidoMaterno": hubspot.properties.n4_3_apellido_materno.value.toUpperCase(),
+                "primerNombre": hubspot.properties.n4_1_nombre.value.toUpperCase(),
+                "fechaNacimiento": birthDate,
+                "RFC": hubspot.properties.n3_rfc.value,
+                "nacionalidad": "MX",
+                "domicilio": {
+                    "direccion": (hubspot.properties.n4_6_calle.value + " " + hubspot.properties.n4_7_num_ext.value).toUpperCase(),
+                    "coloniaPoblacion": hubspot.properties.n4_91_colonia.value.toUpperCase(),
+                    "delegacionMunicipio": hubspot.properties.municipio_de_la_persona.value.replace(".", "", "gi").toUpperCase(),
+                    "ciudad": hubspot.properties.estado_de_la_rep_de_la_persona.value.toUpperCase(),
+                    "estado": estado == 'CIUDAD DE MÉXICO' ? 'CDMX' : estado,
+                    "CP": hubspot.properties.n4_9_c_p_.value
                 }
             };
 
+            let {data} = await axios.post('rccficoscore', params);
+
             return response.json({ 
                 code: 200,
-                params 
+                data 
             });
         } 
         catch(error){
