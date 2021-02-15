49e1f6eb60ec8b2d9baf474b51e69cf2fff727eb
wayasmiguel <wayasmiguel@outlook.com>
2021-02-15 10:10:13 -0600

    feat: open banking response finerio

--- app/http/controllers/openBankingController.js
+++ app/http/controllers/openBankingController.js
@@ -85,6 +85,7 @@ const openBankingController = {
                     }
                     
                     let finerioCredentialAPI = await finerioController.storeCredential(params);
+                    console.log(finerioCredentialAPI);
 
                     if(finerioCredentialAPI.hasOwnProperty('status')){
                         if(finerioCredentialAPI.status == 500){
