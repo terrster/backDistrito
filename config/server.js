64861cf461aecaa2bcd061fe942d8c00a668b2f2
wayasmiguel <wayasmiguel@outlook.com>
2021-06-28 15:02:15 -0500

    netlify domain added

--- config/server.js
+++ config/server.js
@@ -26,7 +26,7 @@ class Server{
 
         this.app.use((request, response, next) => {
             const allowedOrigins = ['https://distritopyme.com', 'https://www.distritopyme.com', 'https://dev.distritopyme.com', 'https://www.dev.distritopyme.com', 'https://impmx.com', 'https://dev.impmx.com', 'https://api-v2.finerio.mx', 'https://distritopyme.netlify.app'];
-            const origin = request.headers.origin;
+            const origin = request.headers.origin;console.log(origin);
             
             response.header('Access-Control-Allow-Origin', '*');
             response.header('Access-Control-Allow-Headers', '*');
